58fcfa8e5dc52c8e43e57999723f0958d766a1b4
Kirby Powell <kpowell6@stedwards.edu>
2015-10-07 18:35:59 -0500

    Halfway through part 4

---
+++ db_downgrade.py
@@ -0,0 +1,10 @@
+#!flask\Scripts\python
+from migrate.versioning import api
+from config import SQLALCHEMY_MIGRATE_REPO
+from config import SQLALCHEMY_DATABASE_URI
+
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+api.downgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, v - 1)
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+
+print('Current DB version: ' + str(v))
